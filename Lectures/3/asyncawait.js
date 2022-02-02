f9de762caf64aaff3de729a16b3cbe6205501c6a
Aughdon Breslin <augh.breslin@gmail.com>
2022-02-01 20:27:56 -0500

    async/callbacks/promises

--- Lectures/3/asyncawait.js
+++ Lectures/3/asyncawait.js
@@ -1,8 +1,49 @@
 
-async function myRide() {
-    return 'Honda CR-V';
+// async function myRide() {
+//     return 'Honda CR-V';
+// }
+
+// function yourRide() {
+//     return Promise.resolve('Honda CR-V');
+// }
+
+// function foo() {
+//     return Promise.reject('Rejected');
+// }
+
+// async function bar() {
+//     throw 'Rejected';
+// }
+
+const weather = true;
+
+async function date() {
+    if (weather) {
+        const dateDetails = {
+            name: 'Cubana Restaurant',
+            location: '55th Street',
+            table: 5
+        };
+        return dateDetails;
+    } else {
+        throw 'Bad weather so no date'
+    }
 }
 
-function yourRide() {
-    return Promise.resolve('Honda CR-V');
+const orderUber = async(details) => {
+    const message = `Get me an Uber ASAP to ${details.location}, we are going on a date.`;
+    return message;
 }
+
+async function myDate() {
+    try {
+        let dateDetails = await date();
+        let message = await orderUber(dateDetails);
+        console.log(message);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+myDate();
+console.log("My date has been called.");
